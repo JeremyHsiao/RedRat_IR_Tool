875aa1adb2a6db44a4b144b868ce661b137fe605
Jeremy Hsiao <jeremy.hsiao@outlook.com>
2017-09-22 15:52:31 +0800

    Add first working code

---
+++ RedRatDBViewer/RedRatDBViewer.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class RedRatDBViewer
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listboxAVDeviceList = new System.Windows.Forms.ListBox();
+            this.listboxRCKey = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rtbSignalData = new System.Windows.Forms.RichTextBox();
+            this.dgvPulseData = new System.Windows.Forms.DataGridView();
+            this.txtFreq = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPulseData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(361, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get RC File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(462, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 41);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Analyze File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listboxAVDeviceList
+            // 
+            this.listboxAVDeviceList.Enabled = false;
+            this.listboxAVDeviceList.FormattingEnabled = true;
+            this.listboxAVDeviceList.ItemHeight = 12;
+            this.listboxAVDeviceList.Location = new System.Drawing.Point(12, 85);
+            this.listboxAVDeviceList.Name = "listboxAVDeviceList";
+            this.listboxAVDeviceList.Size = new System.Drawing.Size(141, 184);
+            this.listboxAVDeviceList.TabIndex = 2;
+            this.listboxAVDeviceList.SelectedIndexChanged += new System.EventHandler(this.listboxAVDeviceList_SelectedIndexChanged);
+            // 
+            // listboxRCKey
+            // 
+            this.listboxRCKey.Enabled = false;
+            this.listboxRCKey.FormattingEnabled = true;
+            this.listboxRCKey.ItemHeight = 12;
+            this.listboxRCKey.Location = new System.Drawing.Point(168, 85);
+            this.listboxRCKey.Name = "listboxRCKey";
+            this.listboxRCKey.Size = new System.Drawing.Size(161, 184);
+            this.listboxRCKey.TabIndex = 3;
+            this.listboxRCKey.SelectedIndexChanged += new System.EventHandler(this.listboxRCKey_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "RC Device List";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(166, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "RC Signal List";
+            // 
+            // rtbSignalData
+            // 
+            this.rtbSignalData.Location = new System.Drawing.Point(12, 503);
+            this.rtbSignalData.Name = "rtbSignalData";
+            this.rtbSignalData.Size = new System.Drawing.Size(564, 148);
+            this.rtbSignalData.TabIndex = 7;
+            this.rtbSignalData.Text = "";
+            // 
+            // dgvPulseData
+            // 
+            this.dgvPulseData.AllowUserToAddRows = false;
+            this.dgvPulseData.AllowUserToDeleteRows = false;
+            this.dgvPulseData.AllowUserToOrderColumns = true;
+            this.dgvPulseData.AllowUserToResizeRows = false;
+            this.dgvPulseData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPulseData.Location = new System.Drawing.Point(12, 291);
+            this.dgvPulseData.Name = "dgvPulseData";
+            this.dgvPulseData.RowTemplate.Height = 24;
+            this.dgvPulseData.Size = new System.Drawing.Size(317, 206);
+            this.dgvPulseData.TabIndex = 8;
+            this.dgvPulseData.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPulseData_CellContentClick);
+            // 
+            // txtFreq
+            // 
+            this.txtFreq.Location = new System.Drawing.Point(364, 89);
+            this.txtFreq.Name = "txtFreq";
+            this.txtFreq.Size = new System.Drawing.Size(211, 22);
+            this.txtFreq.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 663);
+            this.Controls.Add(this.txtFreq);
+            this.Controls.Add(this.dgvPulseData);
+            this.Controls.Add(this.rtbSignalData);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listboxRCKey);
+            this.Controls.Add(this.listboxAVDeviceList);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "RedRat Database Viewer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPulseData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listboxAVDeviceList;
+        private System.Windows.Forms.ListBox listboxRCKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox rtbSignalData;
+        private System.Windows.Forms.DataGridView dgvPulseData;
+        private System.Windows.Forms.TextBox txtFreq;
+    }
+}
+
